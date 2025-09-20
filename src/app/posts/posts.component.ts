16f61aeabfbd42e7a7dea040eafb3c882da1fba9
Tomislav <tomislav25x@hotmail.com>
2025-09-20 20:37:12 +0200

    added Posts component and service

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrl: './posts.component.scss',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PostsComponent {}
